f885d4643ebcbc067d05cefa36ca608f5ba09deb
almahmudzihad <amzihad@gmail.com>
2026-04-03 17:00:52 +0600

    add path for routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,18 @@ import { RouterProvider } from "react-router/dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello from react router</div>,
+    path: '/',
+    element: <div>welcome to react router</div>
   },
-]);
+  {
+    path: 'about',
+    element: <h1>about me router</h1>
+  },
+  {
+    path: 'blogs',
+    element: <h1>all my blogs are here</h1>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
