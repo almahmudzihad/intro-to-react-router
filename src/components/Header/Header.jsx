b627a6a72d132d0ab08dcc6023a2489c92c9ea2f
almahmudzihad <amzihad@gmail.com>
2026-04-03 19:10:01 +0600

    Nav ot link child router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 function Header() {
     return (
         <div>
             <h1>Header Component</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/laptops">Laptops</Link>
+                <Link to="/mobile">Mobile</Link>
+            </nav>
         </div>
     )
 }
