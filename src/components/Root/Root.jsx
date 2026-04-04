98a3d76e7845adcf5a65910ca5381aea646a6375
almahmudzihad <amzihad@gmail.com>
2026-04-04 09:46:39 +0600

    set loding

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Header from '../Header/Header'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigation } from 'react-router'
 import Footer from '../Footer/Footer'
 import Sidebar from '../Sidebar/sidebar'
 import './root.css'
 
 
 function Root() {
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
     return (
         <div>
             <Header />
             <div className='root-main'>
+
                 <Sidebar />
+                {isNavigating && <h1>Loding...</h1>}
                 <Outlet />
             </div>
             
