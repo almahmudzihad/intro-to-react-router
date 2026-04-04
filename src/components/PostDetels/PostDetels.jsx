c937429d147d175ed4017b4dae5a96092dc23a9b
almahmudzihad <amzihad@gmail.com>
2026-04-04 09:35:43 +0600

    go back to post button

--- src/components/PostDetels/PostDetels.jsx
+++ src/components/PostDetels/PostDetels.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { useLoaderData } from 'react-router'
+import { useLoaderData, useNavigate } from 'react-router'
 
 function PostDetels() {
     const post = useLoaderData();
+    const navigate = useNavigate();
     return (
         <div style={{border: '2px solid red', margin: '10px'}}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
+            <button onClick={()=>navigate('/posts')}>Go Back</button>
         </div>
     )
 }
